ef26a9fb416f5960d9847730c18b24c4e3c109b3
lee4339 <skyho4339@naver.com>
2023-09-25 21:35:09 +0900

    키오스크 프로젝트 파일 생성(기본 설계 이후 진행할 예정 )

---
+++ src/main/java/com/ghproject/ghprojectno01/GhprojectNo01Application.java
@@ -0,0 +1,13 @@
+package com.ghproject.ghprojectno01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhprojectNo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhprojectNo01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghproject/ghprojectno01/GhprojectNo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghproject.ghprojectno01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhprojectNo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
